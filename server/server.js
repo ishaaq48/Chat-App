8b12ce2c9e5865a9c3ed31c200eb4a04d7d767a9
Syed Ishaaq <syedishaaq48@gmail.com>
2024-11-29 07:57:12 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const cors = require('cors')
 
 const app = express()
+dotenv.config()
 app.use(cors())
 app.use(express.json())
 
-app.listen(3000, () => {
+PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
     console.log('Server running on 3000...')
 })
